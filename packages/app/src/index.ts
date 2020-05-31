96d79c186c973ada378f75f679bfb5bca45414ab
GalateaPolymorph <galateapolymorph@gmail.com>
2020-05-31 14:00:34 +0200

    Add port cli flag

--- packages/app/src/index.ts
+++ packages/app/src/index.ts
@@ -3,6 +3,27 @@ import fastify from 'fastify';
 import fastifyStatic from 'fastify-static';
 import { serverConfig } from '@peach/backend';
 import path from 'path';
+import meow from 'meow';
+
+const cli = meow(
+  `
+    Usage
+      $ peach <input>
+ 
+    Options
+      --port, -p  Specify port to listen on, default: 3000
+`,
+  {
+    flags: {
+      port: {
+        type: 'number',
+        alias: 'p',
+      },
+    },
+  },
+);
+
+const port = () => (process.env.PORT ? parseInt(process.env.PORT, 10) : cli.flags.port || 3000);
 
 const app = fastify({ logger: { prettyPrint: true } });
 const server = new ApolloServer(serverConfig);
@@ -13,7 +34,7 @@ app.register(fastifyStatic, {
   root: path.resolve(__dirname, '../../frontend/dist'),
 });
 
-app.listen(3000, err => {
+app.listen(port(), err => {
   if (err) {
     app.log.error(err);
     process.exit(1);

748095303c513e50c281ad3e16f0f220a2ccad2c
GalateaPolymorph <galateapolymorph@gmail.com>
2020-05-31 16:32:29 +0200

    Switch to express

--- packages/app/src/index.ts
+++ packages/app/src/index.ts
@@ -1,6 +1,5 @@
-import { ApolloServer } from 'apollo-server-fastify';
-import fastify from 'fastify';
-import fastifyStatic from 'fastify-static';
+import { ApolloServer } from 'apollo-server-express';
+import express from 'express';
 import { serverConfig } from '@peach/backend';
 import path from 'path';
 import meow from 'meow';
@@ -25,18 +24,17 @@ const cli = meow(
 
 const port = () => (process.env.PORT ? parseInt(process.env.PORT, 10) : cli.flags.port || 3000);
 
-const app = fastify({ logger: { prettyPrint: true } });
+const app = express();
 const server = new ApolloServer(serverConfig);
 
-app.register(server.createHandler());
+server.applyMiddleware({ app });
 
-app.register(fastifyStatic, {
-  root: path.resolve(__dirname, '../../frontend/dist'),
+const frontendDistPath = path.join(`${__dirname}/../../frontend/dist`)
+
+app.use('/assets', express.static(frontendDistPath));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(frontendDistPath, 'index.html'));
 });
 
-app.listen(port(), err => {
-  if (err) {
-    app.log.error(err);
-    process.exit(1);
-  }
-});
+app.listen(port(), () => console.log(`🚀 Server ready at http://localhost:${port()}`));

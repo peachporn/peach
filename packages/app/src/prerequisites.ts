6147b0345c0743d7133ff5aed226ad147108dbc3
GalateaPolymorph <galateapolymorph@gmail.com>
2020-06-03 18:02:48 +0200

    Check for ffprobe executable

--- packages/app/src/prerequisites.ts
+++ packages/app/src/prerequisites.ts
@@ -5,11 +5,16 @@ const log = logScope('check-prerequisites');
 
 const hasFFMPEGInstalled = () =>
   which('ffmpeg')
-    .then(() => true)
     .catch(() => {
       log.error('No ffmpeg executable found in PATH! Please make sure you have ffmpeg installed.');
       throw new Error('Missing FFMPEG Executable');
-    });
+    })
+    .then(() => which('ffprobe'))
+    .catch(() => {
+      log.error('No ffprobe executable found in PATH! Please make sure you have ffmpeg installed.');
+      throw new Error('Missing ffprobe Executable');
+    })
+    .then(() => true);
 
 export const arePrerequisitesMet = (): Promise<boolean> =>
-  Promise.all([hasFFMPEGInstalled()]).then(prerequisites => !prerequisites.some(x => x === false));
+  Promise.all([hasFFMPEGInstalled()]).then(prerequisites => !prerequisites.some(x => !x));

cdf1a1537915f6d869b2ee9eed97c0a44ff9b689
GalateaPolymorph <galateapolymorph@gmail.com>
2020-06-10 15:00:03 +0200

    Fix database path

--- src/prisma/index.ts
+++ src/prisma/index.ts
@@ -3,9 +3,6 @@ import * as path from 'path';
 
 export const prisma = new PrismaClient({
   datasources: {
-    db: `file:${path.resolve(__dirname, 'database.db')}`,
-    __internal: {
-      debug: true
-    }
-  },
+    db: 'file:./database.db',
+  }
 });

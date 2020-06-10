72187cd6091e2a6ecd98160a4bd1d0f633e8f003
GalateaPolymorph <galateapolymorph@gmail.com>
2020-06-10 18:24:43 +0200

    Remove debug movie fetch

--- src/app/index.ts
+++ src/app/index.ts
@@ -1,11 +1,8 @@
 import { startServer } from './express';
 import { startTaskWorkers } from './cron';
 import { arePrerequisitesMet } from './prerequisites';
-import { prisma } from '../prisma';
 
 arePrerequisitesMet().then(() => {
   startTaskWorkers();
   startServer();
 });
-
-prisma.movie.findMany().then(console.log);

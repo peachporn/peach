b1561d9b1d3df33e7418490d0424c165c49e471a
GalateaPolymorph <galateapolymorph@gmail.com>
2021-03-04 14:19:12 +0100

    Speed up screencapping significantly

--- @peach/tasks/src/screencaps/take.ts
+++ @peach/tasks/src/screencaps/take.ts
@@ -7,22 +7,33 @@ export type TakeScreencaps = {
   movie: ScreencapMovie;
 };
 
+const ffmpegScreencap = (
+  movie: ScreencapMovie,
+  screencapPath: string,
+  timestamp: string,
+  i: number,
+) =>
+  new Promise(resolve => {
+    ffmpeg(fullPath(movie))
+      .on('end', () => {
+        resolve('SUCCESS' as const);
+      })
+      .screenshots({
+        timestamps: [timestamp],
+        filename: `${movie.id}-0${i}.jpg`,
+        folder: screencapPath,
+      });
+  });
+
 const { createTask, runTask, taskDefinitionOptions } = defineTask<TakeScreencaps>(
   'TAKE_SCREENCAP',
   ({ parameters: { movie } }) =>
-    movieScreencapPath(movie).then(
-      screencapPath =>
-        new Promise(resolve => {
-          ffmpeg(fullPath(movie))
-            .on('end', () => {
-              resolve('SUCCESS' as const);
-            })
-            .screenshots({
-              count: 5,
-              filename: `${movie.id}-%0i.jpg`,
-              folder: screencapPath,
-            });
-        }),
+    movieScreencapPath(movie).then(screencapPath =>
+      Promise.all(
+        ['15%', '30%', '50%', '70%', '85%'].map((timestamp, i) =>
+          ffmpegScreencap(movie, screencapPath, timestamp, i + 1),
+        ),
+      ).then(() => 'SUCCESS'),
     ),
   {
     workers: 1,

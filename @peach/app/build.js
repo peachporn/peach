7af7b1b489bf16a2c831788603861e7118039a58
GalateaPolymorph <galateapolymorph@gmail.com>
2021-05-04 09:02:26 +0200

    Fix prisma client mismatching versions

--- @peach/app/build.js
+++ @peach/app/build.js
@@ -3,7 +3,7 @@ const { build } = require('esbuild');
 build({
   entryPoints: ['./src/index.ts'],
   outfile: '../../dist/app.js',
-  external: ['sharp', 'fluent-ffmpeg'],
+  external: ['sharp', 'fluent-ffmpeg', '_http_common'],
   platform: 'node',
   bundle: true,
   target: 'es2019',

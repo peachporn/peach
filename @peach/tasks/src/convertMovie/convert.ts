afff22837ddc4a1d1e957477e272c1fbcf2376db
GalateaPolymorph <galateapolymorph@gmail.com>
2023-01-31 14:40:40 +0100

    Add quotes to ffmpeg arguments

--- @peach/tasks/src/convertMovie/convert.ts
+++ @peach/tasks/src/convertMovie/convert.ts
@@ -1,21 +1,25 @@
 import * as path from 'path';
 import { execP } from '@peach/utils/src/exec';
+import { logScope } from '@peach/utils/src/logging';
 import { init } from 'ramda';
 import { ConvertableMovie } from './types';
+
+const log = logScope('convert-movie');
 
 export const extensionToMp4 = (file: string) => `${init(file.split('.')).join('.')}.mp4`;
 
 export const convertMovieToMp4 = (movie: ConvertableMovie) => {
   const moviePath = path.join(movie.volume.path, movie.path);
+  const command = [
+    'ffmpeg',
+    `-i "${moviePath}"`,
+    '-c:v libx264',
+    '-crf 23',
+    '-v error',
+    `"${extensionToMp4(moviePath)}"`,
+  ].join(' ');
 
-  return execP(
-    [
-      'ffmpeg',
-      `-i "${moviePath}"`,
-      '-c:v libx264',
-      '-crf 23',
-      '-v error',
-      `${extensionToMp4(moviePath)}`,
-    ].join(' '),
-  );
+  log.info(`Running command: ${command}`);
+
+  return execP(command);
 };

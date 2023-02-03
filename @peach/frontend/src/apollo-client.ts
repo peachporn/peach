dc0f43b3b6d583a84b847bf446b6d8578fd32699
GalateaPolymorph <galateapolymorph@gmail.com>
2023-02-03 11:32:54 +0100

    Add operation name to gql query path for easier debugging

--- @peach/frontend/src/apollo-client.ts
+++ @peach/frontend/src/apollo-client.ts
@@ -1,6 +1,16 @@
-import { ApolloClient, InMemoryCache } from '@apollo/client';
+import { ApolloClient, ApolloLink, concat, HttpLink, InMemoryCache } from '@apollo/client';
+
+const httpLink = new HttpLink({ uri: '' });
+
+const operationNameUriLink = new ApolloLink((operation, forward) => {
+  operation.setContext(() => ({
+    uri: `${window.location.origin}/graphql?operation=${operation.operationName}`,
+  }));
+
+  return forward(operation);
+});
 
 export const client = new ApolloClient({
-  uri: `${window.location.origin}/graphql`,
   cache: new InMemoryCache(),
+  link: concat(operationNameUriLink, httpLink),
 });

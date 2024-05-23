451aafd0a74dd2673f6f41497c273a72d80a7f1c
GalateaPolymorph <galateapolymorph@gmail.com>
2024-05-23 18:00:11 +0200

    Move .env to top, adjust formatting config

--- @peach/apps/storybook/.storybook/main.js
+++ @peach/apps/storybook/.storybook/main.js
@@ -1,25 +1,25 @@
-import { dirname, join } from "path";
+import { dirname, join } from "node:path";
 
 /**
  * This function is used to resolve the absolute path of a package.
  * It is needed in projects that use Yarn PnP or are set up within a monorepo.
  */
 function getAbsolutePath(value) {
-  return dirname(require.resolve(join(value, "package.json")));
+	return dirname(require.resolve(join(value, "package.json")));
 }
 
 /** @type { import('@storybook/react-vite').StorybookConfig } */
 const config = {
-  stories: ["../../../libraries/ui/src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
-  addons: [
-    getAbsolutePath("@storybook/addon-links"),
-    getAbsolutePath("@storybook/addon-essentials"),
-    getAbsolutePath("@chromatic-com/storybook"),
-    getAbsolutePath("@storybook/addon-interactions"),
-  ],
-  framework: {
-    name: getAbsolutePath("@storybook/react-vite"),
-    options: {},
-  },
+	stories: ["../../../libraries/ui/src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
+	addons: [
+		getAbsolutePath("@storybook/addon-links"),
+		getAbsolutePath("@storybook/addon-essentials"),
+		getAbsolutePath("@chromatic-com/storybook"),
+		getAbsolutePath("@storybook/addon-interactions"),
+	],
+	framework: {
+		name: getAbsolutePath("@storybook/react-vite"),
+		options: {},
+	},
 };
 export default config;
--- @peach/apps/storybook/.storybook/preview.tsx
+++ @peach/apps/storybook/.storybook/preview.tsx
@@ -1,25 +1,25 @@
 import { NextUIProvider } from "@nextui-org/react";
-import { Preview } from "@storybook/react";
+import type { Preview } from "@storybook/react";
 import React from "react";
 import "./font.css";
 import "./tailwind.css";
 
 const preview: Preview = {
-  decorators: [
-    (Story) => (
-      <NextUIProvider>
-        <Story />
-      </NextUIProvider>
-    ),
-  ],
-  parameters: {
-    controls: {
-      matchers: {
-        color: /(background|color)$/i,
-        date: /Date$/i,
-      },
-    },
-  },
+	decorators: [
+		(Story) => (
+			<NextUIProvider>
+				<Story />
+			</NextUIProvider>
+		),
+	],
+	parameters: {
+		controls: {
+			matchers: {
+				color: /(background|color)$/i,
+				date: /Date$/i,
+			},
+		},
+	},
 };
 
 export default preview;
